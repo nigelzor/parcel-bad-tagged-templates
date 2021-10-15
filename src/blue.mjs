7b22c31a7caefe7cc9bce0b87e57d7c3d27aa544
Neil Gentleman <ngentleman@gmail.com>
2021-10-15 13:37:37 -0700

    initial commit

---
+++ src/blue.mjs
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const Blue = styled.div`
+    color: blue;
+`;
---
+++ src/index.mjs
@@ -0,0 +1,10 @@
+import {Red} from './red.mjs'
+import {Blue} from './blue.mjs'
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<>
+    <Red>Red</Red>
+    <Blue>Blue</Blue>
+</>, document.getElementById('app'));
